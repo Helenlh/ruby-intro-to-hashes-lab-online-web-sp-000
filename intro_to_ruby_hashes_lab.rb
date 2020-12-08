ec0db2190bff70d0abf9d78c163162266735e5cb
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-08 19:38:50 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,12 +6,12 @@ end
 def actor
   actor = {
     :name => "Dwayne The Rock Johnson"
-    
+  }
 end
 
 def monopoly
 	monopoly = { 
-	  :railroad => 
+	  :railroad =>  
 	  }
 end
 
@@ -24,14 +24,14 @@ def monopoly_with_third_tier
    	monopoly = { 
       :railroads => 
         :pieces => 4,
-        :rent_in_dollars =>
+        :rent_in_dollars =>,
         {
           :one_piece_owned => 25,
           :two_pieces_owned=> 50,
           :three_pieces_owned=>100, 
-          :four_pieces_owned=>200,
+          :four_pieces_owned=>200 
         }
-        :names =>
+        :names =>,
         {
           :reading_railroad =>
           :pennsylvania_railroad =>
@@ -39,36 +39,40 @@ def monopoly_with_third_tier
           :shortline =>
     }
 end
-
+ 
 
 def monopoly_with_fourth_tier 
   monopoly = {
  :railroads => 
-        :pieces => 4,
+        :pieces => 4
         :rent_in_dollars =>
         {
           :one_piece_owned => 25,
           :two_pieces_owned=>50,
           :three_pieces_owned=>100,
           :four_pieces_owned=>200
-        }
-        :names 
+        },
+        :names =>
+        
+      
+        
+        
          { 
            :reading_railroad =>
           {
-            "mortgage_value" => "$100"
-          }
-          :pennsylvania_railroad =>
+            "mortgage_value" => "$100" 
+          },
+          :pennsylvania_railroad => 
           {
            "mortgage_value" =>"$200"
-          }
+          },
           :b_and_o_railroad =>
           {
              "mortgage_value" =>"$400"
-          }
+          },
           :shortline => 
           {
-             "mortgage_value" =>"$800"
+             "mortgage_value" =>"$800"  
           }
 end 
  
