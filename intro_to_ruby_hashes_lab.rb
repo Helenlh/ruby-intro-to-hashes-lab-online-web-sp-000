87538bf951b8eec01b5dbfe48a86bd24de768393
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-09 00:24:52 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,38 +10,33 @@ def actor
 end
 
 def monopoly 
-
-#The key should be :railroads and the value should be an empty hash" do
-  
 	monopoly = { 
 	  :railroad => {}
 	  }
 end
 
 def monopoly_wih_second_tier 
-  #  it "sets the first key of the :railroads hash to a symbol, :pieces, whose value is the integer 4" do
-  monopoly = {
+   monopoly = {
      :railroads => 
+     {
         :pieces => 4,
         :names => {}
-        :rent_in_dollars => {}
-        
-        # it "sets the second key of the :railroads hash to a symbol, :names, whose value is an empty hash" do
-        # it "sets the third key of the :railroads hash to a symbol, :rent_in_dollars, whose value is an empty hash " do
+        :rent_in_dollars => { },
   }
 end
  
 def monopoly_with_third_tier  
    	monopoly = { 
       :railroads => 
+      {
         :pieces => 4,
-        :rent_in_dollars =>,
+        :rent_in_dollars =>
         {
           :one_piece_owned => 25,
           :two_pieces_owned=> 50,
           :three_pieces_owned=>100, 
           :four_pieces_owned=>200 
-        }
+        },
         :names =>,
         {
           :reading_railroad => {}
@@ -55,7 +50,8 @@ end
 def monopoly_with_fourth_tier 
   monopoly = {
  :railroads => 
-        :pieces => 4
+ {
+        :pieces => 4,
         :rent_in_dollars =>
         {
           :one_piece_owned => 25,
@@ -80,6 +76,7 @@ def monopoly_with_fourth_tier
           :shortline => 
           {
              "mortgage_value" =>"$800"
-          }
+          } 
+      }
 end 
  
